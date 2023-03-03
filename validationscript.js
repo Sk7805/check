40ce6b8f95b323ec3d3d5487510866ae0610344b
Sk7805 <126848856+Sk7805@users.noreply.github.com>
2023-03-03 17:16:08 +0530

    Update validationscript.js

--- validationscript.js
+++ validationscript.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function(e) {
 				if(valid) {
 					$("#btnsub").attr("enable", true);
 					jQuery.ajax({
-					url: "/requestform.php",
+					url: "https://github.com/Sk7805/check/edit/main/requestform.php",
 					type: "POST",
 					data:  new FormData(this),
 					contentType: false,
@@ -18,7 +18,7 @@ jQuery(document).ready(function(e) {
 					success: function(data){
                         $("#mail-status").html(data);
 						document.getElementById("contact-form").reset();
-						window.location.href="thankyou.jpeg";
+						window.location.href="https://github.com/Sk7805/check/edit/main/thankyou.jpeg";
 						/*$('#loader-icon').hide();*/
 					},
 					error: function(){} 
